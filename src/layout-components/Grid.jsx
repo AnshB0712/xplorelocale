c5b2c751d4f6a79bdfe916b1fc6d6086fb9540e5
Anshul Borde <89180266+AnshB0712@users.noreply.github.com>
2024-01-31 16:28:23 +0530

    Update Grid.jsx

--- src/layout-components/Grid.jsx
+++ src/layout-components/Grid.jsx
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 export const Grid = styled.div`
       display: grid;
-      grid-template-columns: repeat(auto-fit,minmax(300px,1fr));
+      grid-template-columns: repeat(auto-fill,minmax(340px,1fr));
       justify-content: center;
+      justify-items: center;
       grid-gap: 12px;
 `
